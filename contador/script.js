ccc0a064d088e32bcb79ea107a6a280ba42e63de
laraheloisa <130068472+laraheloisa@users.noreply.github.com>
2024-04-26 11:52:31 -0300

    26/05

--- contador/script.js
+++ contador/script.js
@@ -6,6 +6,7 @@ const divcontador = document.querySelector("[txt-contador]")
 let ehContagemCaracteres = true
 
 
+
 const handleBtnContar = (evento) => {
     console.log(evento.target.value) 
     if(ehContagemCaracteres){
@@ -14,17 +15,23 @@ const handleBtnContar = (evento) => {
     }else {
         evento.target.innerText = "Contar Caracteres"
     }
+
     ehContagemCaracteres = !ehContagemCaracteres 
 }
 
-inputTexto.addEventListener ("input", () => ){ //
-    let contador = 0;
-    if(tipocontagem=="caracteres"){
-    contador = inputTexto.value.length
-    
-    }
+function handleInputTexto(evento){ 
+   
+    if(ehContagemCaracteres){
+        let contadorCaracter = evento.target.value.lengh; // atribui variável, chama a função atribuindo o comprimento da string atualmente digitada no campo de texto à variável 
+        divcontador.innerText = contadorCaracter + "caracteres";} 
 
+    else{
+        let contadorPalavra = evento.target.value.trim().split(" ").length; //trim vai remover espaços extras
+        divcontador.innerText = contadorPalavra + "palavras";
+    }
 }
+
 btnContar.addEventListener("click", handleBtnContar) // capturar um evento 
+inputTexto.addEventListener("input", handleInputTexto)
 //capturar através do evento 
 //todos atributos que começa com on é um evento
