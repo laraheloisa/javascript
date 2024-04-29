16752af29c66c94008d7945682a70d38925ad162
laraheloisa <130068472+laraheloisa@users.noreply.github.com>
2024-04-29 17:06:04 -0300

    29/04

--- contador/script.js
+++ contador/script.js
@@ -5,10 +5,6 @@ const divcontador = document.querySelector("[txt-contador]")
  
 let ehContagemCaracteres = true
 
-<<<<<<< HEAD
-=======
-
->>>>>>> ccc0a064d088e32bcb79ea107a6a280ba42e63de
 
 const handleBtnContar = (evento) => {
     console.log(evento.target.value) 
@@ -22,31 +18,17 @@ const handleBtnContar = (evento) => {
     ehContagemCaracteres = !ehContagemCaracteres 
 }
 
-<<<<<<< HEAD
-inputTexto.addEventListener("input", (evento) => {
-
-    if(tipocontagem === "caracteres") {
-        let contar = evento.target.value.lenght;
-        divContador.innerText = contar; 
-        
-    } else {
-        let contar = evento.target.split('', 1);
-        divContador.innerText = contar
-    }
-})
-=======
 function handleInputTexto(evento){ 
    
     if(ehContagemCaracteres){
-        let contadorCaracter = evento.target.value.lengh; // atribui variável, chama a função atribuindo o comprimento da string atualmente digitada no campo de texto à variável 
-        divcontador.innerText = contadorCaracter + "caracteres";} 
+        let contadorCaracter = evento.target.value.length; // atribui variável, chama a função atribuindo o comprimento da string atualmente digitada no campo de texto à variável 
+        divcontador.innerText = contadorCaracter  + "caracteres";} 
 
     else{
         let contadorPalavra = evento.target.value.trim().split(" ").length; //trim vai remover espaços extras
-        divcontador.innerText = contadorPalavra + "palavras";
+        divcontador.innerText = contadorPalavra  + "palavras";
     }
 }
->>>>>>> ccc0a064d088e32bcb79ea107a6a280ba42e63de
 
 btnContar.addEventListener("click", handleBtnContar) // capturar um evento 
 inputTexto.addEventListener("input", handleInputTexto)
