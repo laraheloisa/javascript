caf9a3583f6da156498aa00968527634ef7b9b58
laraheloisa <130068472+laraheloisa@users.noreply.github.com>
2024-05-08 23:30:19 -0300

    08/05

--- gerador_link/javascript.js
+++ gerador_link/javascript.js
@@ -1,8 +1,38 @@
 const inputTelefone = document.getElementById('input');
 const btngeraLink = document.getElementById('btn-icones');
+const geraLink = document.getElementById('link');
+const copiarLink = document.getElementById('copiar');
 
 btngeraLink.addEventListener('click', function() {
-    const link = document.createElement('a') /*cria um novo elemento HTML <a> (um link)*/
-    link.setAttribute('href', 'https://www.exemplo.com');
-    
+    const telefone = inputTelefone.value;
+
+    if (telefone.trim() !== '') {
+        const linkWhatsApp = `https://wa.me//55${telefone}`;
+        let copiar = 'Clique no link para copiar'
+
+        geraLink.innerHTML = linkWhatsApp; 
+        copiarLink.textContent = copiar;
+   
+    } else {
+        geraLink.textContent = 'Por favor, insira um número de telefone.';
+    }
 })
+
+function clicar() {
+    navigator.clipboard.writeText(inputTelefone.value).then(() => {
+        alert('Copiado para a área de transferência.')
+
+        let copiar = 'Link copiado para área de transferência'
+        copiarLink.textContent = copiar
+    })
+}
+
+inputTelefone.addEventListener('click', function(){
+    copiarLink.textContent = '';
+})
+
+
+//A função writeText() retorna uma promise, que é resolvida quando o texto foi copiado com sucesso, ou rejeitada quando ocorre um erro de permissão de escrita.
+
+//Clipboard.writeText()
+//writeText()
