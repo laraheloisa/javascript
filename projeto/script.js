2b9c844ee56bd6513c4af16b50738317fe26f9d4
laraheloisa <130068472+laraheloisa@users.noreply.github.com>
2024-06-05 11:44:16 -0300

    05/06

--- projeto/script.js
+++ projeto/script.js
@@ -1,6 +1,18 @@
 const btnCriar = document.getElementById("btn-Criar")
 const inputTarefa = document.getElementById("input")
+const divAviso = document.getElementById("aviso")
+const criarDiv = document.getElementById("cria-tarefa")
 
-btnCriar.addEventListener("click", function(){
-
+input.addEventListener("click", function(){
+    const tarefa = input.value;   
+    if(tarefa.trim() !== " "){
+        criarDiv.innerText = tarefa
+    }
+    else{
+        let aviso = "Digite algo para criar uma tarefa";
+        divAviso.textContent = aviso;
+    }
+    
 })
+
+
