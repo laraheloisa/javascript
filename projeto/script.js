ec253aff3887a88fc3e09f4851782f6bc9313589
laraheloisa <130068472+laraheloisa@users.noreply.github.com>
2024-06-11 07:50:37 -0300

    11/06

--- projeto/script.js
+++ projeto/script.js
@@ -1,25 +1,42 @@
-const btnCriar = document.getElementById("btn-Criar")
-const inputTarefa = document.getElementById("input")
-const divAviso = document.getElementById("aviso")
+const inputNovaTarefa = document.getElementById('input')
+const btnCriaTarefa = document.getElementById('btnCriaTarefa')
+const listaTarefas = document.getElementById('listadeTarefas')
 const texto1 = document.getElementById("texto1")
 const texto2 = document.getElementById("texto2")
 
-btnCriar.addEventListener("click", function(){
-    const tarefa = inputTarefa.value.trim();   
-   
-    if(tarefa !== " "){
-        criarDiv.innerText = tarefa;
-        
 
-        texto1.textContent = " "
-        texto2.textContent = " "
+let lista = []
 
-    }
-    else{
-        let aviso = "Digite algo para criar uma tarefa";
-        divAviso.textContent = aviso;
-    }
+btnCriaTarefa.addEventListener('click', function(){
     
+    lista.push(inputNovaTarefa.value) //adiciona um novo elemento no meu array
+    console.log(lista)
+    mostraTarefas()
+    texto1.textContent = " "
+    texto2.textContent = " "
+
+
 })
 
+function mostraTarefas() {
+    let novalista = ' '
 
+    lista.forEach( tarefa => { //passa por cada item do array
+        novalista = novalista + `
+        <li class="task">
+                <div>
+                    <img src="Checked=true, Hover=false.png" alt="checked">
+                </div>
+                <div class='nomeTarefaCriada'>${tarefa}</div>
+                <div>
+                    <button class="btnAcao">
+                        <img src="trash.png" alt="delete">
+                    </button>
+                </div>
+           </li> 
+        `
+    })
+
+    listaTarefas.innerHTML = novalista
+    
+}
